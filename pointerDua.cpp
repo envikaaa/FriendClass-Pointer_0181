d0ee26b466c380ba8a24d50b8b18617edac6a292
envikaaa <envikapriyatna31@gmail.com>
2026-04-27 15:14:24 +0700

    pointer Object mahasiswa

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -10,3 +10,11 @@ public:
         cout << "No Induk =" << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; //pointer Object mahasiswa
+    mhs->nim = 2;
+    mhs->showNim();
+    
+}
