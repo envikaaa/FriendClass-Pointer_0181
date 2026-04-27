d90268b5f2a33b91e2896ef87b48b0d5d4a5754b
envikaaa <envikapriyatna31@gmail.com>
2026-04-27 18:27:09 +0700

    tugasPraktikum

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class siswa;
-class orang;
+class orang
 {
     string nama;
 
@@ -21,3 +21,26 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Kumat");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -6,7 +6,7 @@ class mahasiswa
 private:
     string nama;
 public:
-    friend setNama(mahasiswa &a, string);
+    friend void setNama(mahasiswa &a, string);
 };
 
 void setNama(mahasiswa &a, string b)
