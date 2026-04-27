e3292cf36101a4efe5eaeffc66f7e911b4e48970
envikaaa <envikapriyatna31@gmail.com>
2026-04-27 15:22:47 +0700

    library

---
+++ friendClassSatu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang;
+{
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
